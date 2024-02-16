0152c5c4c07475b0aff4c3bb98c4d95c4756f973
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-15 22:29:36 -0300

    commit message: Update App styling and remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Teste</h1>
+      <h1 className="text-4xl font-bold underline ">Teste</h1>
     </>
   );
 }
