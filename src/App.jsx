2418e1fdbdf3b573627ea74f007384b77b2df53c
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-16 00:09:52 -0300

    Add button component and update styles
    This commit adds a Button component to the src/components/ui directory and imports and renders it in App.jsx.
    
    It also updates the base styles in src/index.css to define color variables for different UI elements and apply theming. This provides a basic light and dark mode and applies border styles globally.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
     <>
       <h1 className="text-4xl font-bold underline ">Teste</h1>
-
+      <Button variant="secondary" className="bg-red-500">Click me</Button>
     </>
   );
 }
