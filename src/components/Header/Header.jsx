0fb25869bc5fa76e96b44ff3e52b65bb7c6f8f76
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-16 23:12:17 -0300

    commit message: Update Header component styling and add new task button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Card, CardContent } from '../ui/card';
-import { CircleUserRound } from 'lucide-react';
+import { Card, CardContent, CardHeader } from '../ui/card';
+import { CircleUserRound, LayoutGrid } from 'lucide-react';
+import { Button } from '../ui/button';
 
 const Header = () => {
   return (
-    <div >
+    <div>
       <Card>
-        <CardContent className='bg-projeto-Roxo'>
-          <CircleUserRound className='text-white'/>
-
-        </CardContent>
+        <CardHeader className="bg-projeto-Roxo h-auto py-8">
+          <div className="flex items-center justify-between pb-6">
+            <LayoutGrid className="text-white" />
+            <p className="text-white">16 de Fevereiro</p>
+            <CircleUserRound className="text-white" />
+          </div>
+          <div className="flex items-center justify-between ">
+            <div className="text-white">
+              <p className="text-2xl">Hoje</p>
+              <p className="text-xs">6 Tasks</p>
+            </div>
+            <Button className="bg-white text-projeto-Roxo">Nova tarefa</Button>
+          </div>
+        </CardHeader>
       </Card>
     </div>
   );
