05d3383718342ceb1c3e14582f3d903b60c7067b
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-16 23:39:51 -0300

    Add Data component to display calendar dates

---
+++ src/components/Data.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from './ui/button';
+
+const Data = () => {
+  return (
+    <div className="flex items-center justify-between p-4">
+      <Button className="bg-projeto-Rosa text-projeto-CinzaEscuro drop-shadow-md p-6">
+        <div className="flex items-center flex-col">
+          <span>16</span>
+          <span>Fev</span>
+        </div>
+      </Button>
+      <Button className="bg-projeto-Rosa text-projeto-CinzaEscuro drop-shadow-md p-6">
+        <div className="flex items-center flex-col">
+          <span>16</span>
+          <span>Fev</span>
+        </div>
+      </Button>
+      <Button className="bg-projeto-Rosa text-projeto-CinzaEscuro drop-shadow-md p-6">
+        <div className="flex items-center flex-col">
+          <span>16</span>
+          <span>Fev</span>
+        </div>
+      </Button>
+      <Button className="bg-projeto-Rosa text-projeto-CinzaEscuro drop-shadow-md p-6">
+        <div className="flex items-center flex-col">
+          <span>16</span>
+          <span>Fev</span>
+        </div>
+      </Button>
+    </div>
+  );
+};
+
+export default Data;
