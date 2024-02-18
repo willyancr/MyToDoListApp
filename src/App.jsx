a3d04ac0d075cb2e3c9046ee8f1617a964aaf461
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-18 09:20:48 -0300

    Add Radix UI checkbox component and render tasks
    This commit adds the Radix UI checkbox component and renders a list of tasks using it. The @radix-ui/react-checkbox package was installed and the Checkbox component was created by wrapping the Radix UI primitive components. The MyTasks component was also added to render sample task data using the new Checkbox. This improves the UI by adding a nice checkbox component for selecting tasks.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Data from './components/Data';
+import MyTasks from './components/MyTasks';
 
 function App() {
   return (
     <div>
       <Header />
       <Data />
+      <MyTasks />
     </div>
   );
 }
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -3,8 +3,8 @@ import { Button } from './ui/button';
 
 const Data = () => {
   return (
-    <div className="flex items-center justify-between pt-4 px-5">
-      <Button className="bg-projeto-Rosa text-projeto-CinzaEscuro drop-shadow-md py-7 px-5 hover:bg-projeto-Rosa">
+    <div className="flex items-center justify-between py-4 px-5">
+      <Button className="bg-projeto-Roxo text-white drop-shadow-md py-7 px-5 hover:bg-projeto-Rosa">
         <div className="flex items-center flex-col">
           <span>16</span>
           <span>Fev</span>
