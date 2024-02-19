4f8e140ac7c9d29f2db345d84c1ada4a8b51d311
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-18 22:32:58 -0300

    Add additional activity options and icons
    This commit adds more activity options (Ideia, Comida, Trabalho, Música) to the ChooseActivity component, along with the corresponding icons from lucide-react. It maps over the new activity array to render multiple cards.

--- src/components/ChooseActivity.jsx
+++ src/components/ChooseActivity.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import { Card } from './ui/card';
-import { ChevronRight, Dumbbell } from 'lucide-react';
+import {
+  ChevronRight,
+  Dumbbell,
+  Lightbulb,
+  UtensilsCrossed,
+  Briefcase,
+  Music4,
+} from 'lucide-react';
 
 const ChooseActivity = () => {
+  const activitys = [
+    { name: 'Esporte', icon: 'Dumbbell', to: '/' },
+    { name: 'Ideia', icon: 'Lightbulb', to: '/' },
+    { name: 'Comida', icon: 'UtensilsCrossed', to: '/' },
+    { name: 'Trabalho', icon: 'Briefcase', to: '/' },
+    { name: 'Música', icon: 'Music4', to: '/' },
+  ];
+  const icons = {
+    Dumbbell: <Dumbbell size={30} />,
+    Lightbulb: <Lightbulb size={30} />,
+    UtensilsCrossed: <UtensilsCrossed size={30} />,
+    Briefcase: <Briefcase size={30} />,
+    Music4: <Music4 size={30} />,
+  };
   return (
     <div className="py-4 px-5">
       <h2 className="text-xl font-bold">Escolha uma atividade</h2>
       <div className="pt-4">
-        <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2">
-          <div className="grid grid-cols-[60px_auto_1fr] items-center gap-1">
-            <div className="text-projeto-Roxo justify-self-center">
-              <Dumbbell />
+        {activitys.map((activity) => (
+          <Card
+            key={activity.name}
+            className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2 mb-4"
+          >
+            <div className="grid grid-cols-[60px_auto_1fr] items-center gap-1">
+              <div className="text-projeto-Roxo justify-self-center">
+                {icons[activity.icon]}
+              </div>
+              <div className=" ">
+                <h3 className="text-xl font-bold">{activity.name}</h3>
+                <p className="text-projeto-CinzaClaro">5 atividades</p>
+              </div>
+              <div className="text-projeto-Roxo justify-self-end ">
+                <ChevronRight size={30} />
+              </div>
             </div>
-            <div className=" ">
-              <h3 className="text-xl font-bold">Esporte</h3>
-              <p className="text-projeto-CinzaClaro">5 atividades</p>
-            </div>
-            <div className="text-projeto-Roxo justify-self-end ">
-              <ChevronRight />
-            </div>
-          </div>
-        </Card>
+          </Card>
+        ))}
       </div>
     </div>
   );
