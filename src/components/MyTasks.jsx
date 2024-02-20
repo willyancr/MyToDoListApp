13cb5411016b6019c2398491f9ba0c77a1f6092c
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-20 11:48:00 -0300

    commit message:
    Add Calender component and refactor MyTasks to map over array of tasks

--- src/components/MyTasks.jsx
+++ src/components/MyTasks.jsx
@@ -6,70 +6,24 @@ const MyTasks = () => {
   return (
     <div className="py-4 px-5">
       <h2 className="text-xl font-bold">Minhas Tarefas</h2>
-      <div className="pt-4">
-        <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2">
-          <div className="flex items-center">
-            <div className="flex-1">
-              <span className="text-sm">6:00 - 7:30</span>
+      {Array.from({ length: 10 }).map((_, index) => (
+        <main className="pt-4" key={index}>
+          <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2">
+            <div className="flex items-center">
+              <div className="flex-1">
+                <span className="text-sm">6:00 - 7:30</span>
+              </div>
+              <div className="flex-1 ">
+                <h3 className="text-xl font-bold">Tarefa 1</h3>
+                <p>Descrição</p>
+              </div>
+              <div className="flex-1 text-right">
+                <Checkbox className="border-projeto-Roxo "></Checkbox>
+              </div>
             </div>
-            <div className="flex-1 ">
-              <h3 className="text-xl font-bold">Tarefa 1</h3>
-              <p>Descrição</p>
-            </div>
-            <div className="flex-1 text-right">
-              <Checkbox className="border-projeto-Roxo "></Checkbox>
-            </div>
-          </div>
-        </Card>
-      </div>
-      <div className="pt-4">
-        <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2">
-          <div className="flex items-center">
-            <div className="flex-1">
-              <span className="text-sm">6:00 - 7:30</span>
-            </div>
-            <div className="flex-1 ">
-              <h3 className="text-xl font-bold">Tarefa 1</h3>
-              <p>Descrição</p>
-            </div>
-            <div className="flex-1 text-right">
-              <Checkbox className="border-projeto-Roxo "></Checkbox>
-            </div>
-          </div>
-        </Card>
-      </div>
-      <div className="pt-4">
-        <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2">
-          <div className="flex items-center">
-            <div className="flex-1">
-              <span className="text-sm">6:00 - 7:30</span>
-            </div>
-            <div className="flex-1 ">
-              <h3 className="text-xl font-bold">Tarefa 1</h3>
-              <p>Descrição</p>
-            </div>
-            <div className="flex-1 text-right">
-              <Checkbox className="border-projeto-Roxo "></Checkbox>
-            </div>
-          </div>
-        </Card>
-      </div>
-      <div className="pt-4">
-        <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2">
-          <div className="flex items-center">
-            <div className="flex-1">
-              <span className="text-sm">6:00 - 7:30</span>
-            </div>
-            <div className="flex-1 ">
-              <h3 className="text-xl font-bold">Tarefa 1</h3>
-              <p>Descrição</p>
-            </div>
-            <div className="flex-1 text-right">
-              <Checkbox className="border-projeto-Roxo "></Checkbox>
-            </div>
-          </div>
-        </Card>
-      </div>
+          </Card>
+        </main>
+      ))}
     </div>
   );
 };
