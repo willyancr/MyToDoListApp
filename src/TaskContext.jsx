b3c857c50d270604e362031450191b594873e14b
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-22 22:44:46 -0300

    Refactor TaskProvider addTask function for better readability
    Refactor the addTask function in TaskProvider to improve readability and
    efficiency. Also, add a loadTasks function to load tasks from localStorage on
    page load.

--- src/TaskContext.jsx
+++ src/TaskContext.jsx
@@ -6,16 +6,30 @@ export const TaskProvider = ({ children }) => {
   const [tasks, setTasks] = React.useState([]);
 
   const addTask = (name, time, description) => {
-    setTasks((prevTasks) => [
-      ...prevTasks,
-      {
+    setTasks((prevTasks) => {
+      const newTask = {
         id: Math.floor(Math.random() * 1000),
         name,
         time,
         description,
-      },
-    ]);
+      };
+      const updatedTasks = [...prevTasks, newTask];
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
   };
+
+  window.addEventListener('load', loadTasks);
+  function loadTasks() {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    } else {
+      localStorage.setItem('tasks', JSON.stringify([]));
+      setTasks([]);
+      console.log('Nenhuma tarefa salva');
+    }
+  }
   const removeTask = (id) => {
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   };
