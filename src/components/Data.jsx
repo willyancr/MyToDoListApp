65706ebdb53bb1ca1b5b6068c8df485635eb5d2a
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-25 21:34:25 -0300

    Add dynamic date display to Data component
    Implemented functionality in the Data component to dynamically display dates for the next five days, including the day and month. This change involves using React's useState and useEffect hooks to generate and set the dates. The month is now localized to Portuguese (Brazil) format, and the dates are displayed in a more user-friendly manner. This enhancement improves the UI by providing users with relevant, up-to-date information.

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 import { Button } from './ui/button';
 
 const Data = () => {
+  const date = new Date();
+  const month = date.toLocaleDateString('pt-BR', { month: 'short' });
+  const [datas, setDatas] = React.useState([]);
+
+  React.useEffect(() => {
+    const datasTemp = [];
+    for (let i = 0; i < 5; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() + i);
+      datasTemp.push(date.toLocaleDateString('pt-BR', { day: 'numeric' }));
+    }
+    setDatas(datasTemp);
+  }, []);
+
   return (
     <div className="flex items-center justify-between py-4 px-5">
-      {Array.from({ length: 5 }).map((_, index) => (
+      {datas.map((data, index) => (
         <Button
           key={index}
           className="bg-projeto-Rosa text-projeto-CinzaEscuro py-7 px-4 hover:bg-projeto-Rosa"
         >
           <div className="flex items-center flex-col">
-            <span className="font-bold">16</span>
-            <span className="text-projeto-CinzaClaro">Fev</span>
+            <span className="font-bold">{data}</span>
+            <span className="text-projeto-CinzaClaro">{month}</span>
           </div>
         </Button>
       ))}
