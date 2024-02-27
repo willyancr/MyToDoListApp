342350b463e91541e21eadc20e2c8406a96d47ec
Willyan Costa <131810535+willyancr@users.noreply.github.com>
2024-02-26 22:06:39 -0300

    Optimize date handling in Data component

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import { Button } from './ui/button';
 
 const Data = () => {
-  //Calcula o mês atual apenas uma vez na montagem do componente
-  const month = React.useMemo(() => {
-    const date = new Date();
-    return date.toLocaleDateString('pt-BR', { month: 'short' });
-  }, []);
-
   const [dates, setDates] = React.useState([]);
-
   React.useEffect(() => {
     const futureDates = [];
     for (let i = 0; i < 5; i++) {
       const date = new Date();
       date.setDate(date.getDate() + i);
-      futureDates.push(date.toLocaleDateString('pt-BR', { day: 'numeric' }));
+      futureDates.push({
+        day: date.toLocaleDateString('pt-BR', { day: 'numeric' }),
+        month: date.toLocaleDateString('pt-BR', { month: 'short' }),
+      });
     }
     setDates(futureDates);
   }, []);
@@ -25,11 +21,11 @@ const Data = () => {
       {dates.map((data) => (
         <Button
           key={data}
-          className="bg-projeto-Rosa text-projeto-CinzaEscuro py-7 px-4 hover:bg-projeto-Rosa"
+          className="bg-projeto-Rosa text-projeto-CinzaEscuro h-14 w-14 hover:bg-projeto-Rosa"
         >
           <div className="flex items-center flex-col">
-            <span className="font-bold">{data}</span>
-            <span className="text-projeto-CinzaClaro">{month}</span>
+            <span className="font-bold">{data.day}</span>
+            <span className="text-projeto-CinzaClaro">{data.month}</span>
           </div>
         </Button>
       ))}
