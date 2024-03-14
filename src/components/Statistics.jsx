881c17c8c2d02d9c5fdc55b9ea280a8beec2754f
Willyan Costa <willyancr@gmail.com>
2024-03-13 22:03:02 -0300

    Add new activity statistics to Statistics component
    Imported useActivitys hook to display new activity statistics in the
    Statistics component. Added new variables to destructure from useTask
    hook. Updated JSX to render new activity statistics cards based on the
    activity data.

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -4,13 +4,17 @@ import {
   LineChart,
   PieChart,
   CheckCircleIcon,
+  Dumbbell,
 } from 'lucide-react';
 import SvgWaves from '@/image/Waves';
 import { Card } from './ui/card';
 import { useTask } from '../TaskContext';
+import { useActivitys } from './hooks/useActivitys';
 
 const Statistics = () => {
-  const { tasks, countCompletedTasks } = useTask();
+  const { tasks, countCompletedTasks, qtdActivity, countTasks } = useTask();
+  const { activitys, icons } = useActivitys();
+
   return (
     <>
       <header className="fixed w-full z-50 top-0 text-projeto-CinzaEscuro">
@@ -24,18 +28,14 @@ const Statistics = () => {
         </div>
       </header>
       <main className="min-h-screen mt-[150px] px-4">
-        <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2 mb-4 static">
+        <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-2 mb-3 ">
           <div className="grid grid-cols-[60px_auto] items-center gap-2 ">
             <div className="text-projeto-Roxo justify-self-center">
               <LineChart size={38} />
             </div>
             <div className="grid grid-cols-1 items-center">
-              <div>
-                <h3 className="text-xl font-bold">Total de tarefas</h3>
-                <p className="text-xl text-projeto-CinzaClaro">
-                  {tasks.length}
-                </p>
-              </div>
+              <h3 className="text-xl font-bold">Total de tarefas</h3>
+              <p className="text-xl text-projeto-CinzaClaro">{tasks.length}</p>
             </div>
           </div>
         </Card>
@@ -45,15 +45,31 @@ const Statistics = () => {
               <CheckCircleIcon size={38} />
             </div>
             <div className="grid grid-cols-1 items-center">
-              <div>
-                <h3 className="text-xl font-bold">Tarefas completas</h3>
-                <p className="text-xl text-projeto-CinzaClaro">
-                  {countCompletedTasks()}
-                </p>
-              </div>
+              <h3 className="text-xl font-bold">Tarefas completas</h3>
+              <p className="text-xl text-projeto-CinzaClaro">
+                {countCompletedTasks()}
+              </p>
             </div>
           </div>
         </Card>
+        <div className="border border-projeto-Rosa/50 rounded mb-4"></div>
+        <div className="grid grid-cols-2 items-center gap-2 mb-4">
+          {activitys.map((activity) => (
+            <Card className="bg-projeto-Rosa text-projeto-CinzaEscuro text-sm px-2 py-1 ">
+              <div className="grid grid-cols-[auto_1fr] items-center gap-3">
+                <div className="text-projeto-Roxo/70">
+                  {icons[activity.icon]}
+                </div>
+                <div>
+                  <h2 className=" font-bold">{activity.name}</h2>
+                  <p className="text-projeto-CinzaClaro">
+                    0{qtdActivity(tasks)[activity.name]}
+                  </p>
+                </div>
+              </div>
+            </Card>
+          ))}
+        </div>
       </main>
     </>
   );
