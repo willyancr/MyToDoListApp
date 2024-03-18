c6347d87c48dfbd457a8d97e095327ebd395859f
Willyan Costa <willyancr@gmail.com>
2024-03-17 21:43:01 -0300

    Update input styling in FilterCards component

--- src/components/FilterCards.jsx
+++ src/components/FilterCards.jsx
@@ -8,7 +8,10 @@ const FilterCards = () => {
   return (
     <div className="pt-4">
       <div className="flex items-center justify-between px-5 ">
-        <Input className="pl-10 relative" placeholder="Pesquisar tarefas..." />
+        <Input
+          className="pl-10 relative border-projeto-CinzaClaro/50 outline-none py-6"
+          placeholder="Pesquisar tarefas..."
+        />
         <Search className="absolute left-7 text-projeto-CinzaClaro/75" />
       </div>
       <div className="flex gap-4 py-3 px-5 ">
