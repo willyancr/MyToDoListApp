32a23d5a0cf1dec378169dbfad2b80e58c1590c1
Willyan Costa <willyancr@gmail.com>
2024-03-17 21:36:56 -0300

    Add new Label and Switch components
    Added new Label and Switch components to the project for better UI
    functionality and design consistency. The components are based on
    @radix-ui/react-label and @radix-ui/react-switch packages.

---
+++ src/components/FilterCards.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Search } from 'lucide-react';
+import { Input } from './ui/input';
+import { Switch } from './ui/switch';
+import { Label } from './ui/label';
+
+const FilterCards = () => {
+  return (
+    <div className="pt-4">
+      <div className="flex items-center justify-between px-5 ">
+        <Input className="pl-10 relative" placeholder="Pesquisar tarefas..." />
+        <Search className="absolute left-7 text-projeto-CinzaClaro/75" />
+      </div>
+      <div className="flex gap-4 py-3 px-5 ">
+        <div className="flex items-center gap-1">
+          <Switch id="completas" />
+          <Label htmlFor="completas">Completas</Label>
+        </div>
+        <div className="flex items-center gap-1">
+          <Switch id="incompletas" />
+          <Label htmlFor="incompletas">Incompletas</Label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterCards;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './Header';
-import Data from './Data';
 import MyTasks from './MyTasks';
+import FilterCards from './FilterCards';
 
 const Home = () => {
   return (
     <div>
       <Header className="fixed w-full z-50 top-0" />
       <main className="min-h-screen mt-[160px]">
-        <Data />
+        <FilterCards />
         <MyTasks />
       </main>
     </div>
