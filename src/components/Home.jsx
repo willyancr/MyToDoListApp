7468ec7ee4658a214ba127f72d59df0ab47f1f47
Willyan Costa <willyancr@gmail.com>
2024-03-18 22:38:34 -0300

    Remove FilterCards component from Home.jsx
    The FilterCards component was removed from the Home.jsx file as it is no longer needed. This change simplifies the component structure.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import MyTasks from './MyTasks';
-import FilterCards from './FilterCards';
 
 const Home = () => {
   return (
     <div>
       <Header className="fixed w-full z-50 top-0" />
       <main className="min-h-screen mt-[160px]">
-        {/* <FilterCards /> */}
         <MyTasks />
       </main>
     </div>
